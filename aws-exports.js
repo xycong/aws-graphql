ba810d73973964706bbb11b135ec2688f7954bda
Victor Cong <xycong@uwaterloo.ca>
2018-03-09 17:03:20 -0500

    Exported AppSync config and initialized GraphQL client

---
+++ aws-exports.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://oliw75mc3bhqddn3daquzmv3ai.appsync-api.us-west-2.amazonaws.com/graphql",
+    "region": "us-west-2",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-sp2yi3lkvvet7iihxjaau6vw7m"
+}
