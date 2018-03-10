d2f3ca5ac0c5829e69876b237595e7dc37d9d579
Victor Cong <xycong@uwaterloo.ca>
2018-03-10 13:03:36 -0500

    Created city class and navigation between screens

--- src/AddCity.js
+++ src/AddCity.js
@@ -1,7 +1,62 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import uuidV4 from 'uuid/v4'
+import Input from './Input'
 import { compose, graphql } from 'react-apollo'
 import CreateCityMutation from './mutations/CreateCity'
+import ListCities from './queries/ListCities';
+
+const initialState = {
+  name: '',
+  country: ''
+}
 
 class AddCity extends React.Component {
+  state = initialState;
+
+  onChangeText(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addPost() {
+    this.props.onAdd({
+      ...this.state,
+      id: uuidV4()
+    })
+    this.props.navigation.navigate('Cities');
+    this.setState(initialState);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Input
+          onChangeText={this.onChangeText.bind(this)}
+          placeholder='Name'
+          type='name'
+          value={this.state.name}
+        />
+        <Input
+          onChangeText={this.onChangeText.bind(this)}
+          placeholder='Country'
+          type='country'
+          value={this.state.country}
+        />
+        <Button
+          title="Add City"
+          onPress={this.addPost.bind(this)}
+        />
+      </View>
+    );
+  }
 }
  
 export default compose(
@@ -22,3 +77,26 @@ export default compose(
     });
   });
 )(AddCity)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#ededed',
+    marginVertical: 10
+  }
+});
--- src/Cities.js
+++ src/Cities.js
@@ -1,14 +1,71 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView
+} from 'react-native'
 import { compose, graphql } from 'react-apollo'
 import ListCities from './queries/ListCities'
 import NewCitiesSubscription from './subscriptions/NewCitySubscription';
+import { ListItem } from 'react-native-elements';
+import { StackNavigator } from 'react-navigation';
+import City from './City';
  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  headerImage: {
+    width: '100%',
+    height: 40
+  },
+  listItem: {
+    borderBottomColor: '#ededed'
+  }
+})
+
 class Cities extends React.Component {
+  static navigationOptions = {
+    headerTitle: (
+      <Image
+        resizeMode='contain'
+        style={styles.headerImage}
+        source={require('./assets/citieslogo.png')}
+      />
+    )
+  }
 }
  
-export default compose(
+const CitiesWithData = compose(
   graphql(ListCities, {
       props: props => ({
         cities: props.data.listCities ? props.data.listCities.items : [],
       })
   })
-)(CityList)
+)(Cities)
+
+const routes = {
+  CitiesWithData: {
+    screen: CitiesWithData
+  },
+  City: {
+    screen: City
+  }
+}
+
+const config = {
+  navigationOptions: {
+    headerTintColor: 'white',
+    headerStyle: {
+      backgroundColor: '#0091EA'
+    },
+    headerTitleStyle: {
+      color: 'white'
+    }
+  }
+}
+
+export default StackNavigator(routes, config)
