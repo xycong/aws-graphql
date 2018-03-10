d181f4c1942b1d27645b8559736face7375a20f8
Victor Cong <xycong@uwaterloo.ca>
2018-03-10 12:39:12 -0500

    Added subscription on new city create

--- src/AddCity.js
+++ src/AddCity.js
@@ -1,1 +1,17 @@
-// TODO
+import { compose, graphql } from 'react-apollo'
+import AddCityMutation from './mutations/AddCity'
+ 
+class AddCity extends React.Component {
+  // class definition here
+  // now have access to this.props.onAdd()
+}
+ 
+export default compose(
+  graphql(AddCityMutation, {
+    props: props => ({
+      onAdd: city => props.mutate({
+        variables: city
+      })
+    })
+  })
+)(AddCity)
